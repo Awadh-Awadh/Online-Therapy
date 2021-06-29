1f581747dd2eab11d51accb5b4139f7e194549af
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-06-29 11:07:00 +0300

    validate the form

--- static/js/ft-calls.js
+++ static/js/ft-calls.js
@@ -1,14 +1,31 @@
 let submit = document.querySelector('.submit-btn');
-let people = document.querySelector('#Fname').value;
-let error = document.querySelector('.error');
-let phone = document.querySelector('#phone').value;
-let mail = document.querySelector('#mail').value;
+let people = document.querySelector('#Fname');
+let errorN = document.querySelector('.errorN');
+let errorM = document.querySelector('.errorM');
+let errorP= document.querySelector('.errorP');
+let mail = document.querySelector('#mail');
+let phone = document.querySelector('#phone');
+
 
 
 function validate(){
-  if(mail ==''){
-    error.innerText = "this field cannot be empty";
-    error.style.opacity = 1
+  if(people.value.length < 1){
+    errorN.style.opacity = 1
+    errorN.innerText = "This field cannot be empty"
+  }
+ else if(mail.value.length<3 ){
+    errorM.style.opacity = 1
+    errorM.innerText = "This field cannot be empty"
+  }
+  else if(phone.value.length < 10 || phone.value.length >10){
+    errorP.style.opacity = 1
+    errorP.innerText = "Enter correct phone number"
+  }
+  else{
+    errorM.style.opacity=0
+    errorN.style.opacity=0
+    errorP.style.opacity=0
+    alert("We have recorded your Response. You will receive your call on you scheduled details")
   }
   
 }
