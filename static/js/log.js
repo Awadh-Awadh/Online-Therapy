cda4fd0005fcb63f78389280890c22d4817822dd
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-06-29 20:57:24 +0300

    add validation function

---
+++ static/js/log.js
@@ -0,0 +1,18 @@
+let mail = document.querySelector('#log-email')
+let password = document.querySelector('#log-password');
+let submitButton = document.querySelector('.log-btn')
+
+function validateLogIn(){
+  if(mail.value === '' || password.value === ''){
+    alert("please fill the all the field")
+  }
+  else{
+    location.replace("./index.html")
+
+  }
+}
+
+submitButton.addEventListener('click',(e)=>{
+  validateLogIn();
+  e.preventDefault()
+})
