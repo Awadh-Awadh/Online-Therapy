b906d4973149e07169e7413e73c5700f9f78e592
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-06-30 11:06:21 +0300

    added a redirect message

--- static/js/redirect.js
+++ static/js/redirect.js
@@ -4,5 +4,5 @@ let redirectHome = document.querySelector('.btn-home')
 
 redirectHome.addEventListener('click',(e)=>{
   e.preventDefault();
-  location.replace("")
+  location.replace("./index.html")
 })
